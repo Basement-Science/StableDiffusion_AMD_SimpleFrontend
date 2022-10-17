58f1587add7c644da5c31dd7440a3d4e35ac3131
Basement Science <BaseSc@protonmail.com>
2022-10-17 21:17:02 +0200

    Major update: Support for other models (Waifu Diffusion), Automatic Model rebuilding when relevant parameters were changed.

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Supports text2image only. Untested on operating systems other than Windows.
 - Logs all ***prompts*** used for each image file additionally in a text file `output.log`. This way you can recreate your images - or variations of them - later on.
 - Allows varying any prompt or parameter combinations and enqueueing them as a batch to be generated. Includes instructions on how to do so.
 - Can be aborted at any time.
+- **V2:** Support for other models (Waifu Diffusion)
+- **V2:** Automatic Model rebuilding when relevant parameters were changed
 
 ## How to set up
 1. Follow instructions to generate your first image using the [harishanand95](https://github.com/harishanand95/diffusers/tree/dml) model. See for example [this guide](https://gitgudblog.vercel.app/posts/stable-diffusion-amd-win10).
 2. If you ran into any problems, resolve them now. 
-3. Download this repository as zip, and copy `simpleFrontend.py` and `dml_onnx_SF.py` from this repository to your `diffusers\examples\inference` folder. <br>
+3. Download this repository as zip, and copy the `*.py` files from this repository to your `diffusers\examples\inference` folder. <br>
 If you prefer to use `git clone`, you could clone this repo into another folder and symlink the script files over.
 
 ## How to use
@@ -25,13 +27,16 @@ If you prefer to use `git clone`, you could clone this repo into another folder 
     - `getInferenceSteps()` -> Number of steps taken to '_refine_' the image. 
     - `getGscale()` -> This number changes details - or adds or removes them - in an image. Important for finalizing an image to your taste.
 <br><br>
-3. Once everything is set up, save the file and simply run `simpleFrontend.py` in the Terminal with the activated python environment. The script will show some progress info. <p>
+1. Below these functions, the following line specifies which **model** to use and at which **resolution**. You can change this like in the commented lines below it.
+   ```python
+   model_settings = ModelSettings(width = 512, height = 512, model = "sd14")
+   ```
+2. Once everything is set up, save the file and simply run `simpleFrontend.py` in the Terminal with the activated python environment. The script will show some progress info. <p>
 You dont have to run the script until completion - if you want to change anything, abort the script by pressing `ctrl+c` in the Terminal, make changes in the script and run it again _(dont forget to save)_. You can also edit the script while it is still running - The changes take effect the next time you start it.</p>
-4. Your generated images are stored in the `outputs` folder in `diffusers\examples\inference`. 
+5. Your generated images are stored in the `outputs` folder in `diffusers\examples\inference`. 
 <br>The filename tells you the parameters except the promt text. The prompt can be found in `output.log` outside the `outputs` folder. The `run` number tells you whether images were generated in the same batch or not.
 
 ## Notes
-- The script will store some variables in the file `saveState.pickle`. Without this, it will loose track of image number and run number and could overwrite existing image files.
-- By default, image resolution is 512x512. On AMD cards most higher resolutions cannot be generated at the moment or have issues. But if you want to try, follow the instructions at the very end of `simpleFrontend.py`. 
-On my RX 6800 XT I am able to use 768x512 resolution, but no higher.
-- `dml_onnx_SF.py` contains almost no changes over [harishanand95's example script](https://github.com/harishanand95/diffusers/blob/dml/examples/inference/dml_onnx.py). It only omits the generation of an example image at the end. You can have `dml_onnx_SF.py` and the original `dml_onnx.py` side by side this way. If you want, you can make this modification yourself in the original and then change the first line of `simpleFrontend.py` to import from that file instead.
+- The script will store some variables in the file `saveState.pickle`. Without this, it will loose track of image number and run number and could overwrite existing image files. Additionally, `modelSettings.pickle` is used to store information about the currently compiled model. Without it, the model will be recompiled on next run.
+- By default, image resolution is 512x512. On AMD cards most higher resolutions cannot be generated at the moment or have issues. However 768x512 should work, provided you have enough VRAM. 
+- `dml_onnx_SF.py` and `save_onnx_SF.py` contain almost no changes over [harishanand95's example script](https://github.com/harishanand95/diffusers/blob/dml/examples/inference/dml_onnx.py). They only omit the generation of an example image at the end, or the actual generation of the model data in the latter file. You can have these files and their original (non-`_SF` versions) side by side this way. If you want, you can make these modification yourself in the original and then change the first lines of `simpleFrontend.py` to import from those files instead.
--- simpleFrontend.py
+++ simpleFrontend.py
@@ -1,33 +1,69 @@
 from dml_onnx_SF import *
+from save_onnx_SF import convert_to_onnx
 import pickle
 import random
-
-# load or create file that holds some persistent variables
-saveFileName = "saveState.pickle"
-imageNumber = 1
-runCount = 1
-logFileName = "output.log"
-try:
-    saveFile = open(saveFileName, "rb")
-    imageNumber = pickle.load(saveFile)
-    try: 
-        runCount = pickle.load(saveFile)
-    except (EOFError) as e: # file does not contain 2nd variable
-        #print(f"exception: {e}")
-        saveFile = open(saveFileName, "wb")
-        pickle.dump(runCount, saveFile)
-except (OSError, IOError, EOFError) as e: # file does not exist / is not accessible
-    #print(f"exception: {e}")
-    saveFile = open(saveFileName, "wb")
-    pickle.dump(imageNumber, saveFile)
-#print(f"imageNumber: {imageNumber}, runCount: {runCount}")
-
-try: 
-    logFile = open(logFileName, "a")
-except (OSError, IOError) as e:
-    print("unable to write to log file")
+import hashlib
+import os
+from typing import Optional
 
 # ########################################################################################## #
+# More compatible models can potentially be added here in future
+models = { # dictionary of short display names for diffusion models
+        "sd14": "CompVis/stable-diffusion-v1-4",
+        "wd": "hakurei/waifu-diffusion"
+    }
+# ########################################################################################## #
+    
+class ModelSettings:
+    def INVALID(): return ModelSettings(width = -1, height = -1)
+    
+    def __init__(self, 
+        width: Optional[int] = 512,
+        height: Optional[int] = 512,
+        model: Optional[str] = list(models.keys())[0] # first key entry in models
+    ):
+        self.width = width
+        self.height = height
+        if not model in models.keys():
+            raise ValueError("unknown model was specified.")
+        self.model = model
+        self.storedInPath = "onnx/"
+        self.fileChecksum = "INVALID"
+        
+    # define equality
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height and self.model == other.model and self.storedInPath == other.storedInPath and self.fileChecksum == other.fileChecksum
+        
+    def __ne__(self, other):
+        return not self.__eq__(other)
+        
+    def generateModelChecksum(self):
+        hashAlgo = hashlib.sha256()
+        
+        lastLength = 0
+        count = 0
+        
+        # Iterate through all files under source folder
+        for path, dirs, files in os.walk(self.storedInPath):
+            for file_name in files:
+                filePath = os.path.join(self.storedInPath, file_name)
+                
+                count += 1
+                line = f"[{count}/{len(files)}] - {filePath}"
+                
+                print(line.ljust(lastLength, ' '), end='\r')
+                lastLength = len(line)
+                with open(filePath, "rb") as file:
+                    bytes = file.read()  # read file as bytes
+                    # add every file and file path to the hash
+                    hashAlgo.update(bytes)
+                hashAlgo.update(filePath.encode('utf-8'))
+        
+        print()
+        self.fileChecksum = hashAlgo.hexdigest()
+        return self.fileChecksum
+# end class
+
 class simpleFrontend:
     def createImage(self, prompt, seed, inferenceSteps = 50, Gscale = 7.5):
         global imageNumber
@@ -35,10 +71,10 @@ class simpleFrontend:
         #if self.lastSeed != seed or self.lastGscale != Gscale: # causes problems sometimes for whatever reason.
         torch.manual_seed(seed)
             
-        outputFilePath = f"outputs/img_{imageNumber}_seed{seed}_steps{inferenceSteps}_Gscale{Gscale}_run{runCount}.png"
+        outputFilePath = f"outputs/img_{imageNumber}_{self.modelSettings.model}_seed{seed}_steps{inferenceSteps}_Gscale{Gscale}_run{runCount}.png"
         print(f"File will be: {outputFilePath}")
         
-        image = self.pipe(prompt, height=512, width=512, num_inference_steps=inferenceSteps, guidance_scale=Gscale, eta=0.0, execution_provider="DmlExecutionProvider")["sample"][0]
+        image = self.pipe(prompt, height=self.modelSettings.height, width=self.modelSettings.width, num_inference_steps=inferenceSteps, guidance_scale=Gscale, eta=0.0, execution_provider="DmlExecutionProvider")["sample"][0]
         image.save(outputFilePath)
         print("image saved successfully.")
         
@@ -46,48 +82,106 @@ class simpleFrontend:
         logFile.flush()
         
         imageNumber += 1
-        self.lastSeed = seed
-        self.lastGscale = Gscale
     # end createImage
     
-    def __init__(self):
-        self.lastSeed = -1
-        self.lastGscale = -1
-        def dummy_checker(images, **kwargs): return images, False
+    def __init__(self, model_settings: ModelSettings):
+        self.modelSettings = model_settings
+        modelPath = models[self.modelSettings.model]
         self.lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
-        self.pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=self.lms, use_auth_token=True)
-        # self.pipe.safety_checker = dummy_checker # disable nsfw filter - not sure if this works.
+        self.pipe = StableDiffusionPipeline.from_pretrained(modelPath, scheduler=self.lms, use_auth_token=True)
+        self.pipe.safety_checker = lambda images, **kwargs: (images, False) # disable nsfw filter - does not seem to work or be necessary.
+        
+        print("generating checksum for Model in " + self.modelSettings.storedInPath)
+        print("Checksum is: " + str(self.modelSettings.generateModelChecksum()))
+        
+        try:
+            if (currentModelState != self.modelSettings):
+                self.compileModel()
+        except AttributeError:
+            print("Current model seems to be faulty.")
+            self.compileModel()
     # end __init__
+    
+    def compileModel(self):
+        print("generating text parsing model...")
+        torch.manual_seed(42)
+        text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", return_dict=False)
+        
+        # start conversion
+        print(f"generating model with width: {self.modelSettings.width}, height: {self.modelSettings.height}, model: {self.modelSettings.model}. This will take some time.")
+        convert_to_onnx(self.pipe.unet, self.pipe.vae.post_quant_conv, self.pipe.vae.decoder, text_encoder, height=self.modelSettings.height, width=self.modelSettings.width, outputPath=self.modelSettings.storedInPath)
+        
+        print("generating checksum for Model in " + self.modelSettings.storedInPath)
+        print("Checksum is: " + str(self.modelSettings.generateModelChecksum()))
+        with open(modelStateFileName, "wb") as saveFile:
+            pickle.dump(self.modelSettings, saveFile)
+    # end compileModel
+# end class
+
+# load or create files that holds some persistent variables
+saveFileName = "saveState.pickle"
+imageNumber = 1
+runCount = 1
+logFileName = "output.log"
+modelStateFileName = "modelSettings.pickle"
+currentModelState = ModelSettings.INVALID()
+try:
+    saveFile = open(saveFileName, "rb")
+    imageNumber = pickle.load(saveFile)
+    try: 
+        runCount = pickle.load(saveFile)
+    except (EOFError) as e: # file does not contain 2nd variable
+        saveFile = open(saveFileName, "wb")
+        pickle.dump(runCount, saveFile)
+except (OSError, IOError, EOFError) as e: # file does not exist / is not accessible / does not contain as much as expected
+    saveFile = open(saveFileName, "wb")
+    pickle.dump(imageNumber, saveFile)
+
+try:
+    modelStateFile = open(modelStateFileName, "rb")
+    currentModelState = pickle.load(modelStateFile)
+except (OSError, IOError, EOFError) as e: # file does not exist / is not accessible / does not contain as much as expected
+    print("current model state could not be determined.")
+
+try: 
+    logFile = open(logFileName, "a")
+except (OSError, IOError) as e:
+    print("unable to write to log file")
 
 # ########################################################################################## #
 # Set up task(s) in these functions below:
 # return an array of values you want to use for each parameter. Uncomment one return line per function.
 # NOTE: you can abort the script using ctrl+c in the console window at any time.
 def getPrompts():
-    return [
-    "a photo of an astronaut riding a horse on mars"]
+    tokens = ["blue"]
+    return [f"a huge {token} and white sci-fi cyberpunk robot raptor with 2 legs, walking through a big lake, water splashing and waves, super detailed and intricate, golden ratio, sharp focus" for token in tokens]
 
 def getSeeds():
-    #return [0] # single value
+    return [459] # single value
     #return [464, 488, 764123, 55] # multiple specific values
-    #return [i + 42 for i in range(10)] # range of values, starting at an offset
-    return [random.randint(0, 999999) for i in range(2)] # a bunch of random value
+    #return [i + 44 for i in range(6)] # range of values, starting at an offset
+    #return [random.randint(0, 999999) for i in range(5)] # a bunch of random value
     #return [78, 79, 80, 83, 86] + [i + 65 for i in range(4)] # combination of above
     
 def getInferenceSteps():
-    return [40] # single value
-    #return [35, 45, 50, 60] # multiple specific values
+    return [30] # single value
+    #return [25, 35, 50, 75] # multiple specific values
     #return [15 + (i*5) for i in range(10)] # range of values
 
 def getGscale():
-    return [6.5] # single value
-    #return [4 + (i*2) for i in range(6)]
-    #return [5.0 + (i*0.5) for i in range(4)]
+    return [8.5] # single value
+    #return [5 + (i*2) for i in range(3)]
+    #return [5.0 + (i*0.5) for i in range(10)]
 
 # ########################################################################################## #
 # run the combinations of parameters. No caching is done, so each combination will take its time.
 try:
-    FE = simpleFrontend()
+    # Specify the model and parameters. Changing these will require recompilation of the model.
+    model_settings = ModelSettings(width = 512, height = 512, model = "sd14")
+    #model_settings = ModelSettings(width = 768, height = 512, model = "sd14")
+    #model_settings = ModelSettings(width = 768, height = 512, model = "wd")
+    
+    FE = simpleFrontend(model_settings)
     
     for prompt in getPrompts():
         print(f"prompt: {prompt}")
@@ -102,7 +196,4 @@ finally: # at program exit, save persistent variables to 'saveFile'
         pickle.dump(runCount + 1, saveFile)
 
 # Works on AMD Windows platform
-# Image width and height is set to 512x512
-# If you need images of other sizes (size must be divisible by 8), make sure to save the model with that size using save_onnx.py (last lines)
-# For example, if you need width=768 and height=512, change save_onnx.py with width=768 and height=512 and also change it in this file at line 41
 # Default values are height=512, width=512, num_inference_steps=50, guidance_scale=7.5, eta=0.0, execution_provider="DmlExecutionProvider"
