f72709be0c3115043330e4daae17cf94ab873fb2
Basement Science <BaseSc@protonmail.com>
2022-10-29 01:14:06 +0200

    introduced PromptPermutationSet class - better work batch support
    Added PromptPermutationSet_fromPreviousImgNumber() to parse log entries and regenerate using previous prompts.
    added stable diffusion 1.5 option

--- simpleFrontend.py
+++ simpleFrontend.py
@@ -1,15 +1,23 @@
+# Works on AMD GPU + Windows platform
+# NOTE: you can abort the script using ctrl+c in the console window at any time.
+
+from ast import Num
 from dml_onnx_SF import *
 from save_onnx_SF import convert_to_onnx
 import pickle
 import random
 import hashlib
 import os
-from typing import Optional
+from typing import Iterable, Optional
+from numbers import Number
+import re
+import linecache
 
 # ########################################################################################## #
 # More compatible models can potentially be added here in future
 models = { # dictionary of short display names for diffusion models
         "sd14": "CompVis/stable-diffusion-v1-4",
+        "sd15": "runwayml/stable-diffusion-v1-5",
         "wd": "hakurei/waifu-diffusion"
     }
 # ########################################################################################## #
@@ -103,8 +111,9 @@ class simpleFrontend:
     # end __init__
     
     def compileModel(self):
+        print("Starting model compilation process. This will take some time.")
         print("generating text parsing model...")
-        torch.manual_seed(42)
+        #torch.manual_seed(42) # not needed
         text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", return_dict=False)
         
         # start conversion
@@ -118,6 +127,92 @@ class simpleFrontend:
     # end compileModel
 # end class
 
+# gotta love weakly typed languages, sigh.
+def wrapInList(arg) -> list: 
+    if isinstance(arg, type([])):
+        return arg
+    elif isinstance(arg, Iterable) and not isinstance(arg, str):
+        return list(arg)
+    else:
+        out = []
+        out.append(arg)
+        return out
+
+# gotta love weakly typed languages, sigh.
+def verifyListType(list: list, t : type) -> list:
+    for e in list:
+        if not isinstance(e, t):
+            raise TypeError(f"Type Verification failed. Expected type: {t}, actual type: {type(e)}")
+    return list
+        
+class PromptPermutationSet:
+    def __init__(self, prompts, seeds, inferenceSteps, guideScales):
+        self.prompts = verifyListType(wrapInList(prompts), str)
+        self.seeds = verifyListType(wrapInList(seeds), Number)
+        self.inferenceSteps = verifyListType(wrapInList(inferenceSteps), Number)
+        self.guideScales = verifyListType(wrapInList(guideScales), Number)
+    
+    def runPrompt(self, frontEnd: simpleFrontend) -> None:
+        for prompt in self.prompts:
+            print(f"prompt: {prompt}")
+            for seed in self.seeds:
+                for inferenceSteps in self.inferenceSteps:
+                    for G_scale in self.guideScales:
+                        frontEnd.createImage(prompt, seed, inferenceSteps, G_scale)
+    
+    def __str__(self) -> str:
+        return f"prompts: {self.prompts}, seeds: {self.seeds}, inferenceSteps: {self.inferenceSteps}, guideScales: {self.guideScales}"
+# end class
+
+# regexes for parsing the output log back into prompts
+logLineParseRegex         = r"^[^#\/]*\/.+_\d+_.*"
+imgNumExtractRegex        = r"(?<=.\/img_)\d+(?=_)"
+paramSplitterRegex_prompt = r"(?<=(\.png \t)).*"
+paramSplitterRegex_seed   = r"(?<=_seed)\d+(?=_)"
+paramSplitterRegex_steps  = r"(?<=_steps)\d+(?=_)"
+paramSplitterRegex_Gscale = r"(?<=_Gscale)\d+(\.\d+)?(?=_)"
+
+# this cannot be a static function of PromptPermutationSet because snake is dumb AF
+def PromptPermutationSet_fromPreviousImgNumber(imgNum) -> PromptPermutationSet:
+    low = 1
+    high = 0
+    # count the lines in the file.
+    with open (logFileName,'rb') as f:
+        for line in f:
+            high += 1
+
+    # perform binary search over the entries in log file.
+    while True:
+        if low <= high:
+            mid = (high + low) // 2
+
+            # evaluate line's image number using regex
+            line = linecache.getline(logFileName, mid)
+            imgNumAtLine = int(re.search(imgNumExtractRegex, line).group())
+
+            # If imgNum is greater, ignore left half
+            if imgNumAtLine < imgNum:
+                low = mid + 1
+    
+            # If imgNum is smaller, ignore right half
+            elif imgNumAtLine > imgNum:
+                high = mid - 1
+
+            else: # we are done. Result is in 'line'.
+                break
+        else: # the element was not present
+            raise LookupError(f"could not find information about image {imgNum} using binary search.")
+
+    return PromptPermutationSet(
+        prompts = str(re.search(paramSplitterRegex_prompt, line).group()), 
+        seeds = int(re.search(paramSplitterRegex_seed, line).group()), 
+        inferenceSteps = int(re.search(paramSplitterRegex_steps, line).group()), 
+        guideScales = float(re.search(paramSplitterRegex_Gscale, line).group())
+        )
+# end function
+
+# ########################################################################################## #
+# --- Start Execution ---
 # load or create files that holds some persistent variables
 saveFileName = "saveState.pickle"
 imageNumber = 1
@@ -149,51 +244,43 @@ except (OSError, IOError) as e:
     print("unable to write to log file")
 
 # ########################################################################################## #
-# Set up task(s) in these functions below:
-# return an array of values you want to use for each parameter. Uncomment one return line per function.
-# NOTE: you can abort the script using ctrl+c in the console window at any time.
-def getPrompts():
-    tokens = ["blue"]
-    return [f"a huge {token} and white sci-fi cyberpunk robot raptor with 2 legs, walking through a big lake, water splashing and waves, super detailed and intricate, golden ratio, sharp focus" for token in tokens]
-
-def getSeeds():
-    return [459] # single value
-    #return [464, 488, 764123, 55] # multiple specific values
-    #return [i + 44 for i in range(6)] # range of values, starting at an offset
-    #return [random.randint(0, 999999) for i in range(5)] # a bunch of random value
-    #return [78, 79, 80, 83, 86] + [i + 65 for i in range(4)] # combination of above
-    
-def getInferenceSteps():
-    return [30] # single value
-    #return [25, 35, 50, 75] # multiple specific values
-    #return [15 + (i*5) for i in range(10)] # range of values
-
-def getGscale():
-    return [8.5] # single value
-    #return [5 + (i*2) for i in range(3)]
-    #return [5.0 + (i*0.5) for i in range(10)]
-
+# Define the work to be done here. --------------------------------------------------------- #
+# Default values are height=512, width=512, inferenceSteps=50, guideScales=7.5, eta=0.0, execution_provider="DmlExecutionProvider"
 # ########################################################################################## #
-# run the combinations of parameters. No caching is done, so each combination will take its time.
+workSets = []
+# add a simple prompt - single line, easy to comment out
+workSets.append(PromptPermutationSet(prompts = "a huge blue and white sci-fi cyberpunk robot raptor with 2 legs, walking through a big lake, water splashing and waves, super detailed and intricate, golden ratio, sharp focus", seeds = 459, inferenceSteps = 30, guideScales = 8.5))
+
+# create a more complex prompt - comment out the '.append' line to ignore.
+ps1 = PromptPermutationSet(
+    prompts = "background dark, block houses, eastern Europe, city highly detailed oil painting, unreal 5 render, rhads, bruce pennington, studio ghibli, tim hildebrandt, digital art, octane render, beautiful composition, trending on artstation, award-winning photograph, masterpiece",
+    seeds = [42], 
+    #seeds = [i + 43 for i in range(16)], # range of values, starting at an offset
+    #seeds = [random.randint(0, 999999) for i in range(5)], # a bunch of random value
+    #seeds = [78, 79, 80, 83, 86] + [i + 65 for i in range(4)], # combination of above
+    inferenceSteps = 50, 
+    #inferenceSteps = [40, 20, 10], 
+    guideScales = 7.5 
+    #guideScales = [5 + (i*2) for i in range(3)]
+    #guideScales = [7 + (i*0.5) for i in range(6)]
+    ) # end append
+#workSets.append(ps1)
+
+# add a prompt to re-generate an image based on the log file. Simply provide the image number.
+#workSets.append(PromptPermutationSet_fromPreviousImgNumber(1))
+
 try:
-    # Specify the model and parameters. Changing these will require recompilation of the model.
+    # Specify the model and parameters. Changing these will recompile the model which will take time.
     model_settings = ModelSettings(width = 512, height = 512, model = "sd14")
     #model_settings = ModelSettings(width = 768, height = 512, model = "sd14")
     #model_settings = ModelSettings(width = 768, height = 512, model = "wd")
+    #model_settings = ModelSettings(width = 768, height = 512, model = "sd15")
     
     FE = simpleFrontend(model_settings)
-    
-    for prompt in getPrompts():
-        print(f"prompt: {prompt}")
-        for seed in getSeeds():
-            for inferenceSteps in getInferenceSteps():
-                for G_scale in getGscale():
-                    FE.createImage(prompt, seed, inferenceSteps, G_scale)
+    for pps in workSets:
+        pps.runPrompt(FE)
 finally: # at program exit, save persistent variables to 'saveFile'
     logFile.close()
     with open(saveFileName, "wb") as saveFile:
         pickle.dump(imageNumber, saveFile)
         pickle.dump(runCount + 1, saveFile)
-
-# Works on AMD Windows platform
-# Default values are height=512, width=512, num_inference_steps=50, guidance_scale=7.5, eta=0.0, execution_provider="DmlExecutionProvider"
